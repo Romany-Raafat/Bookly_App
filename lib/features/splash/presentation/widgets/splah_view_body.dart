ac6ebab5244099724e46b048883edd02594f9093
Romany Raafat <romanyraafat96@gmail.com>
2025-11-28 04:25:17 +0200

    splash view ui

--- lib/features/splash/presentation/widgets/splah_view_body.dart
+++ lib/features/splash/presentation/widgets/splah_view_body.dart
@@ -1,10 +1,23 @@
+import 'package:bookly_app_project/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(
+          AssetsData.logo,
+          height: 80,
+          width: 100,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app_project/constants.dart';
 import 'package:bookly_app_project/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -11,7 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: kPrimaryColor),
+      debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
   }
