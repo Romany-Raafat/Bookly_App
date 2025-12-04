ccce5a6a89f445b395d81436d8c4539e25292df7
Romany Raafat <romanyraafat96@gmail.com>
2025-12-04 02:11:39 +0200

    implement fetch newst book

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app_project/features/home/data/models/book_model/book_mod
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,30 @@
+import 'dart:js_interop';
+
 import 'package:bookly_app_project/core/errors/failure.dart';
+import 'package:bookly_app_project/core/utils/api_service.dart';
 import 'package:bookly_app_project/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app_project/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            "volumes?Filtering=free-ebooks&q=subject:crime%20fiction&Sorting=newst",
+      );
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
